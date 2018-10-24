270d5c0361871e621bd74f9b26212909b5efa120
Jonathan Caiza <jpaul.snp@gmail.com>
2018-10-24 11:01:27 -0500

    Lectura de archivos y escritura
    Escritura y lectura en javascript

--- 01-Javascript/05-callbacks.js
+++ 01-Javascript/05-callbacks.js
@@ -3,8 +3,14 @@ const contenidoAAgregar = 'Jonathan\n';
 const nombreArchivo = '05-texto.txt';
 console.log('Inicio');
 
-fs.readFile('04-operadores.js','utf-8',
+fs.readFile(nombreArchivo,'utf-8',
     (error, contenidoArchivo) =>{ //Callback
+
+        // fs.writeFile(nombreArchivo, contenidoArchivo + contenidoAAgregar, (err) => {
+        //     if(err) throw err
+        //     console.log('Se logro agregar')
+        // });
+
     if(error) {
         console.error(error);
         try {
@@ -15,8 +21,18 @@ fs.readFile('04-operadores.js','utf-8',
         console.log('Extra');
     }else{
         console.log('Si sirvio', contenidoArchivo);
+        fs.writeFile(nombreArchivo, contenidoArchivo + contenidoAAgregar, (err) => {
+            if(err) throw err
+            console.log('Se logro agregar')
+        });
     }
 });
+
+// fs.writeFile(nombreArchivo, contenidoAAgregar, (err) => {
+//     if(err) throw err
+//     console.log('Se logro agregar')
+// });
+
 
 console.log('Fin');
 
