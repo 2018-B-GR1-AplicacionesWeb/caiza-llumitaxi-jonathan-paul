0d13349c7f0b5c0415ad4ab46392a336bdefd723
Jonathan Caiza <jpaul.snp@gmail.com>
2018-11-12 11:02:41 -0500

    Http metodos

--- 03-http/videos/src/main.ts
+++ 03-http/videos/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import {} from 'http-server'  //EN Typescript para importar
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
